94679162e2e73e47988e7fc6ba5fd5af3909b589
Michael Munga <micknyalle@gmail.com>
2025-04-21 14:34:26 +0300

    impliment usestate and events

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Item from "./Item";
+import { useState } from "react";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+  const filteredItems = items.filter(
+    (item) => selectedCategory === "All" || item.category === selectedCategory
+  );
   return (
     <div className="ShoppingList">
-      <div className="Filter">
+      <div
+        className="Filter"
+        value={selectedCategory}
+        onChange={handleCategoryChange}
+      >
         <select name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
@@ -13,7 +25,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
